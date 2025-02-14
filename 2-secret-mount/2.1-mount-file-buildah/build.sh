bf1783304e576584fab7b4477133f425ddee1aa8
Mike Croft <backtotheoldhouse@gmail.com>
2025-02-14 17:32:01 +0000

    update custom build strategy

--- 2-secret-mount/2.1-mount-file-buildah/build.sh
+++ 2-secret-mount/2.1-mount-file-buildah/build.sh
@@ -6,21 +6,13 @@ cd /tmp/input
 # build framework
 TAG="${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}"
 
-if [[ -z "${DOCKERFILE_URL}" ]]; then
-  # do nothing
-else
-  curl -L "${DOCKERFILE_URL}" -o /tmp/input/Dockerfile
-fi
-
-# performs the build of the new image defined by dockerfile.sample
-buildah --storage-driver vfs bud ${BUILD_OPTS} --isolation chroot -t ${TAG} .
-
-
 # buildah requires a slight modification to the push secret provided by the service
 # account to use it for pushing the image
 cp /var/run/secrets/openshift.io/push/.dockercfg /tmp
 (echo "{ \"auths\": " ; cat /var/run/secrets/openshift.io/push/.dockercfg ; echo "}") > /tmp/.dockercfg
 
+# performs the build of the new image defined by dockerfile.sample
+buildah --tls-verify=false --storage-driver vfs bud ${BUILD_OPTS} --isolation chroot -t ${TAG} .
 
 # push the new image to the target for the build
 buildah --storage-driver vfs push ${PUSH_OPTS} --tls-verify=false --authfile /tmp/.dockercfg ${TAG}
--- 2-secret-mount/2.1-mount-file-buildah/secret-mount-file.Dockerfile
+++ 2-secret-mount/2.1-mount-file-buildah/secret-mount-file.Dockerfile
@@ -1,12 +1,14 @@
-FROM registry.redhat.io/ubi9/ubi-minimal:9.5
+FROM image-registry.openshift-image-registry.svc:5000/leak-test/custom-buildah-image:latest
 
 ENV AWS_CREDS=.aws/credentials
 
 RUN mkdir .aws && \
     microdnf install -y python
 
-RUN --mount=type=secret,id=awscreds,dst=$AWS_CREDS \
-    cat $AWS_CREDS
+RUN --mount=type=secret,id=AWS_ACCESS_KEY_ID \
+    --mount=type=secret,id=AWS_SECRET_KEY \
+    echo "AWS_ACCESS_KEY_ID=$(cat /run/secrets/AWS_ACCESS_KEY_ID)" && \
+    echo "AWS_SECRET_KEY=$(cat /run/secrets/AWS_SECRET_KEY)"
 
 RUN --mount=type=secret,id=ghtoken,dst=.env \
     cat .env
